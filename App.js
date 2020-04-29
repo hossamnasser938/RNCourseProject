f97e1c5188cc1b1cefb1bab5a75e0dd5e6e67b94
Hossam <hossamnasser938@gmail.com>
2020-04-29 05:05:57 +0200

    Module 7 _ Lesson 1

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {Order} from './src/components/Order';
-import {dummyOrder} from './src/utils/dummyData';
-import {OrdersScreen} from './src/screens/Orders';
+import {IonIcon} from './src/components/IonIcon';
+
+const users = [
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'AboBakr', phone: '01165826473'},
+  {name: 'Hossam', phone: '01023415623'},
+  {name: 'John', phone: '0111947464'},
+];
 
 class App extends React.Component {
   render() {
-    return <OrdersScreen />;
+    return (
+      <View style={styles.container}>
+        {users.map(user => {
+          const {name, phone} = user;
+
+          return (
+            <View style={styles.userContainer}>
+              <View style={styles.wrapper}>
+                <IonIcon style={styles.icon} name="person" />
+                <Text style={styles.text}>{name}</Text>
+              </View>
+              <View style={styles.wrapper}>
+                <IonIcon style={styles.icon} name="call" />
+                <Text style={styles.text}>{phone}</Text>
+              </View>
+            </View>
+          );
+        })}
+      </View>
+    );
   }
 }
 
@@ -15,6 +65,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  userContainer: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: 'green',
+  },
+  icon: {
+    fontSize: 32,
+    paddingEnd: 10,
+  },
+  text: {
+    fontSize: 18,
   },
 });
 
