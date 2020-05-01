70cb41fc40f109bf9caca79644c4425489b858b3
Hossam <hossamnasser938@gmail.com>
2020-05-01 05:06:13 +0200

    Module 8 _ Lesson 2

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {OrdersScreen} from './src/screens/Orders';
+import {NavigationContainer} from '@react-navigation/native';
 
 class App extends React.Component {
   render() {
-    return <OrdersScreen />;
+    return (
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Text>React Native Course</Text>
+        </View>
+      </NavigationContainer>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
