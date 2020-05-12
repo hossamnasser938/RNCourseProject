e8c06eface469a58212e8aaf9c00d8078df2db9f
Hossam <hossamnasser938@gmail.com>
2020-05-12 05:53:56 +0200

    Module 11 _ Lesson 9

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import authReducer from './reducers/authReducer';
 
 const rootReducer = combineReducers({auth: authReducer});
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export default store;
